9abd1c125d46ab5e1adeb4a6f455e02ecd7d756f
harsh-devjs <harsh.devjs@gmail.com>
2025-07-12 11:33:05 +0530

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,34 @@
+import Image from 'next/image'
 import React from 'react'
+
+const footerImages = [
+    {
+        id: 1,
+        img: '/1.png'
+    },
+    {
+        id: 2,
+        img: '/2.png'
+    },
+    {
+        id: 3,
+        img: '/3.png'
+    },
+    {
+        id: 4,
+        img: '/4.png'
+    },
+]
 
 const Footer = () => {
     return (
-        <footer>
-            Footer
+        <footer className='flex justify-between items-center h-16'>
+            <div className="text-sm">&copy;2025 NextCMS. All rights reserved.</div>
+            <div className="flex items-center gap-3">
+                {footerImages.map(img => (
+                    <Image src={img.img} alt='' width={15} height={15} key={img.id} />
+                ))}
+            </div>
         </footer>
     )
 }
