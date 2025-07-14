35b3b622510482e54e43488a3fb9f85baa53cb98
harsh-devjs <harsh.devjs@gmail.com>
2025-07-14 17:13:48 +0530

    create Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Button = ({ text, url }) => {
+    return (
+        <Link href={url} className={`py-2 px-3 bg-green-600 text-gray-50 rounded cursor-pointer lg:w-fit font-light`}>
+            {text}
+        </Link>
+    )
+}
+
+export default Button
