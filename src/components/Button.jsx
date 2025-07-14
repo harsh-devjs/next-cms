7261b4d0a235c767af997f51d5404578b6d1cff2
harsh-devjs <harsh.devjs@gmail.com>
2025-07-14 17:35:59 +0530

    change Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Button = ({ text, url }) => {
     return (
-        <Link href={url} className={`py-2 px-3 bg-green-600 text-gray-50 rounded cursor-pointer lg:w-fit font-light`}>
+        <Link href={url} className={`py-2 px-3 bg-green-600 text-gray-50 rounded cursor-pointer w-fit font-light`}>
             {text}
         </Link>
     )
